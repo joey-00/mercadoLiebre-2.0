187f4a316a5b59a496837dcaf6d20bc8f526c167
jose acevedo <joseacevedo410@gmail.com>
2023-02-11 05:50:44 -0300

    se repara problema en products.ejs

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -4,7 +4,7 @@ const path = require('path');
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-const toThousand = n => n.tofixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const toThousand = n => n.toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
 	// Root - Show all products
